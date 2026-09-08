b3a321263c8582e7e6ddd146b441ee324462aeaf
jeniferfl <jenifer.alamea251963@potros.itson.edu.mx>
2026-09-07 22:33:03 -0700

    Aqui taaa
    👍

---
+++ src/main/java/com/example/MiPortafolio/MiPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiPortafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPortafolioApplication.class, args);
+	}
+
+}
